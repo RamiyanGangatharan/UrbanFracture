7beeea4e9d640420779c6c75aee7267b281253d1
RamiyanGangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2025-05-17 14:08:56 -0400

    compass UI

---
+++ Assets/Scripts/UI/HUD/CompassUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CompassBar : MonoBehaviour
+{
+    public RectTransform compassBarTransform;
+    public Transform cameraTransform;
+
+    [System.Serializable]
+    public class CompassMarker
+    {
+        public string direction;
+        public RectTransform marker;
+        public Vector3 worldDirection;
+    }
+
+    public List<CompassMarker> markers;
+
+    void Update()
+    {
+        foreach (var marker in markers)
+        {
+            UpdateMarker(marker);
+        }
+    }
+
+    private void UpdateMarker(CompassMarker markerData)
+    {
+        Vector3 flatForward = new Vector3(cameraTransform.forward.x, 0, cameraTransform.forward.z).normalized;
+        Vector3 flatMarkerDir = markerData.worldDirection.normalized;
+
+        float angle = Vector3.SignedAngle(flatForward, flatMarkerDir, Vector3.up);
+        float normalizedAngle = angle / 180f; // -1 (left) to 1 (right)
+
+        float halfWidth = compassBarTransform.rect.width / 2f;
+        float xPos = normalizedAngle * halfWidth;
+
+        markerData.marker.anchoredPosition = new Vector2(xPos, 0);
+    }
+}
